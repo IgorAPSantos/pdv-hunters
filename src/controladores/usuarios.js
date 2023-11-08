34653c5224f9c6e41ab153ea7938655ce4833fa6
IgorAPSantos <igorppandre@hotmail.com>
2023-11-08 12:21:39 -0300

    final primeira sprint

--- src/controladores/usuarios.js
+++ src/controladores/usuarios.js
@@ -6,7 +6,7 @@ const cadastrarUsuario = async (req, res) => {
     try {
         const emailJaExiste = await knex('usuarios').where({ email }).first()
         if (emailJaExiste) {
-            return res.status(400).json('Este email já está cadastrado.')
+            return res.status(400).json({ mensagem: 'Este email já está cadastrado.' })
         }
         const senhaCriptografada = await bcrypt.hash(senha, 10)
 
@@ -17,7 +17,7 @@ const cadastrarUsuario = async (req, res) => {
         }).returning('*')
 
         if (!novoUsuario) {
-            return res.status(400).json('Não foi possível cadastrar o usuário, tente novamente.')
+            return res.status(400).json({ mensagem: 'Não foi possível cadastrar o usuário, tente novamente.' })
         }
 
         const { senha: _, ...dadosUsuario } = novoUsuario[0]
@@ -36,7 +36,37 @@ const detalharPerfilUsuarioLogado = async (req, res) => {
     return res.json(req.usuario);
 }
 
+const editarUsuario = async (req, res) => {
+    const { nome, email, senha } = req.body
+    const { id } = req.usuario
+
+    try {
+        const emailJaExiste = await knex('usuarios').where({ email }).andWhere('id', '!=', id).first()
+
+        if (emailJaExiste) {
+            return res.status(400).json({ mensagem: "Este email ja existe no banco de dados" })
+        }
+
+        const senhaCriptografada = await bcrypt.hash(senha, 10)
+
+        const usuarioEditado = await knex('usuarios').update({ nome, email, senha: senhaCriptografada }).where({id}).returning(['id', 'nome', 'email'])
+
+        if (!usuarioEditado[0]) {
+            return res.status(400).json({ mensagem: "Não foi possível atualizar o usuário" })
+        }
+
+        return res.status(200).json({ atuliazadoComSucesso: usuarioEditado[0] })
+
+    }
+
+    catch (error) {
+        console.log(error)
+        return res.status(500).json({ mensagem: "Erro interno do servidor" })
+    }
+}
+
 module.exports = {
     cadastrarUsuario,
-    detalharPerfilUsuarioLogado
+    detalharPerfilUsuarioLogado,
+    editarUsuario
 }
